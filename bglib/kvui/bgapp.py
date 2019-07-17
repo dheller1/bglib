a05341dd4ee3801164f58cf67ac108fa5c91c4e5
dheller1 <dominik.heller1@gmail.com>
2019-07-17 18:34:23 +0200

    add default window size

--- bglib/kvui/bgapp.py
+++ bglib/kvui/bgapp.py
@@ -1,11 +1,15 @@
 from kivy.app import App
+from kivy.core.window import Window
 from bglib.kvui.rootwidget import RootWidget
 
 
 class BgApp(App):
+    WindowSize = (1280, 800)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.rootwidget = RootWidget()
+        Window.size = BgApp.WindowSize
+        self.rootwidget = RootWidget(size=BgApp.WindowSize)
 
     def build(self):
         return self.rootwidget
